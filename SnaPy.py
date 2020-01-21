e22448d1e5ede7303599cdd31e7cfe07be462fa8
YongTat <tanyongtat.tyt@gmail.com>
2020-01-21 16:24:12 +0800

    Tested on self created text documents

--- SnaPy.py
+++ SnaPy.py
@@ -1,16 +1,38 @@
 import os
+from random import randint
 
-def print_path():
+def get_path():
     path = os.getcwd()
-    print("Your current location: " + path)
+    return path
 
 def list_dir():
     folderlist = os.listdir()
-    print(folderlist)
+    return folderlist
+
+def list_filter():
+    cwdlist = list_dir()
+    safelist = [".git", "README.md", "SnaPy.py"]
+    for items in safelist:
+        cwdlist.remove(items)
+    return cwdlist
+
+def file_remove(filename):
+    path = get_path()
+    fullpath = os.path.join(path, filename)
+    os.remove(fullpath)
+
+def remove_half():
+    cwdlist = list_filter()
+    lenlist = len(cwdlist)
+    half = lenlist/2
+    while len(cwdlist) > half:
+        filetoremove = cwdlist[randint(0,len(cwdlist)-1)]
+        file_remove(filetoremove)
+        cwdlist.remove(filetoremove)
 
 def main():
-    print_path()
-    list_dir()
+    list_filter()
+    remove_half()
 
 if __name__ == "__main__":
     main()
